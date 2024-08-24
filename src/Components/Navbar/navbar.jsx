1bd1447de07c437edd4be26bfb7441484a56fe70
Adan Khan <adanjavedkhan67@gmail.com>
2024-08-24 16:54:16 +0500

    Nav Fix

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useRef } from "react";
 import "./Navbar.css";
+
 import sprite from "../../assets/Icons/sprite.svg";
 import { GiHamburgerMenu, GiCancel } from "react-icons/gi";
-import { BsCart } from "react-icons/bs";
-import { FaUserCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import Cart from "../Cart/cart";
 
-const CustomNavbar = () => {
+//<--===========Redux Imports ==========--->
+import { useDispatch } from "react-redux";
+import { cartOpen } from "../../State/Cart/cart.slice";
+
+export default function CustomNavbar() {
+  const dispatch = useDispatch();
   const [menuActive, setMenuActive] = useState(false);
   const menuRef = useRef(null);
 
@@ -14,50 +19,56 @@ const CustomNavbar = () => {
     setMenuActive(!menuActive);
   };
 
+  const openCart = () => {
+    handleNavToggle();
+    dispatch(cartOpen());
+  };
+
   return (
-    <div
-      className={`custom-navbar-wrapper ${menuActive ? "overlay-active" : ""}`}
-    >
-      <nav className="fixed-top-bar">
-        <div className="nav-container">
-          <div className="logo-section">
-            <h1>
-              Sole<span>Stash</span>
-            </h1>
-          </div>
-          <div
-            className="menu-panel"
-            ref={menuRef}
-            style={{ left: menuActive ? "0" : "-40rem" }}
-          >
-            <div className="menu-top">
-              <GiCancel className="close-btn" onClick={handleNavToggle} />
+    <>
+      <Cart></Cart>
+      <div
+        className={`custom-navbar-wrapper ${
+          menuActive ? "overlay-active" : ""
+        }`}
+      >
+        <nav className="fixed-top-bar">
+          <div className="nav-container">
+            <div className="logo-section">
+              <h1>
+                Sole<span>Stash</span>
+              </h1>
             </div>
-            <div className="nav-items">
-              <div className="nav-item">
-                <Link to="/" className="nav-link">
-                  Home
-                </Link>
+            <div
+              className="menu-panel"
+              ref={menuRef}
+              style={{ left: menuActive ? "0" : "-40rem" }}
+            >
+              <div className="menu-top">
+                <GiCancel className="close-btn" onClick={handleNavToggle} />
               </div>
-              <div className="nav-item">
-                <Link to="/contact" className="nav-link">
+              <div className="nav-items">
+                <div className="nav-item">
+                  <Link to="/" className="nav-link">
+                    Home
+                  </Link>
+                </div>
+                <div className="nav-item" onClick={openCart}>
                   <svg>
                     <use xlinkHref={`${sprite}#icon-shopping-basket`} />
                   </svg>
-                </Link>
-              </div>
-              <div className="nav-item">
-                <Link to="/profile" className="nav-link">
-                  <p>Profile</p>
-                </Link>
+                </div>
+                <div className="nav-item">
+                  <Link to="/profile" className="nav-link">
+                    <p>Profile</p>
+                  </Link>
+                </div>
               </div>
             </div>
+            <GiHamburgerMenu className="menu-btn" onClick={handleNavToggle} />
           </div>
-          <GiHamburgerMenu className="menu-btn" onClick={handleNavToggle} />
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
+    </>
   );
-};
-
-export default CustomNavbar;
+}
