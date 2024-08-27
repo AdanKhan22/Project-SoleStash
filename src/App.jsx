4240843e86a2f19182bbbe7e60f0543a89c4e136
Adan Khan <adanjavedkhan67@gmail.com>
2024-08-27 22:37:12 +0500

    Added Vercel Insights (Test)

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ import {
 } from "./homepage/index.js";
 import Cart from "./Components/Cart/cart.jsx";
 import React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function App() {
   return (
     <>
+      <SpeedInsights />
       <Cart />
       <Header />
       <NewArrival></NewArrival>
