3547118ad679ca401f3b937515d2627fe0d5f7e7
Adan Khan <adanjavedkhan67@gmail.com>
2024-08-28 21:26:54 +0500

    Routing Bug Fix

--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,17 @@ import store from "../Redux/Store/store.js";
 import { Provider } from "react-redux";
 import { AuthProvider } from "../Firebase/Context/authContext/index.jsx";
 
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <Provider store={store}>
+    <Provider store={store}>
+      <AuthProvider>
+        <SpeedInsights />
+        <Analytics />
         <RouterProvider router={router} />
-      </Provider>
-    </AuthProvider>
+      </AuthProvider>
+    </Provider>
   </React.StrictMode>
 );
